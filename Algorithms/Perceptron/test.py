83d46e12974222e3765e22ee2e8b509ce57edf83
Deviprasad B R L <deviprasadbrl7@gmail.com>
2026-04-22 22:30:55 +0530

    Update test.py

--- Algorithms/Perceptron/test.py
+++ Algorithms/Perceptron/test.py
@@ -2,7 +2,7 @@ from algo import Perceptron
 import pandas as pd
 
 df=pd.read_csv(r'placement.csv')
-
+//Filling the missing data in the dataset
 df["cgpa"] = df["cgpa"].fillna(df["cgpa"].mean())
 df["iq"] = df["iq"].fillna(df["iq"].mean())
 
@@ -12,5 +12,6 @@ y=df["placement"].values
 model=Perceptron()
 model.fit(x,y)
 print(model.parameters())
+//From the dataset
 test=[8.1,166]
 print(model.predict(test))
